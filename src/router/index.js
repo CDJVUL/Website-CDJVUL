618af22b2e4491f319c86f8d543fa00b79b5e698
Louis-Philippe Seguin <lpseguin978@proton.me>
2026-08-30 13:35:32 -0400

    Fix issue multiple import

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,12 @@
-import contact from "@/components/contact/contact.vue";
 import { createRouter, createWebHashHistory } from "vue-router";
+import Contact from "@/components/contact/contact.vue";
 import Evenements from "../components/evenements/Evenements.vue";
 import Home from "../components/home/Home.vue"
 import Information from "../components/information/Information.vue"
+import Partenaires from "../components/partenaires/Partenaires.vue"
 import ProjectOverview from "../components/projects/ProjectOverview.vue"
 import Projects from "../components/projects/Projects.vue"
 import Responsables from "../components/responsables/Responsables.vue"
-import partenaires from "../components/partenaires/Partenaires.vue"
-
 
 const router = createRouter({
     history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -43,14 +42,14 @@ const router = createRouter({
             component: Evenements
         },
         {
-            path: "/partenaires",
+            path: "/Partenaires",
             name: "PartenairesPage",
-            component: partenaires
+            component: Partenaires
         },
         {
-            path: "/contact",
+            path: "/Contact",
             name: "ContactPage",
-            component: contact
+            component: Contact
         },
     ],
     scrollBehavior (to, from, savedPosition) {
